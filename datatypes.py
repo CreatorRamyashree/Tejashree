a632159c2a136f7e58e323d35c9b498e2e36d94e
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-01-10 17:35:42 +0100

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+a= 8
+print(type(a))
+
+f= float(a)
+print(type(f))
+
+b= 5.6
+print(type(b))
+
+s= str(b)
+print(type(s))
+
+c= "House"
+print(type(c))
+
+d= False 
+print(type(d))
+
---
+++ stringoperations.py
@@ -0,0 +1,20 @@
+str1= "Tejashree"
+str2= "Ramyashree"
+
+print("str1 + str2 = ", str1 + str2)
+
+Str = "School"
+print(Str[::3])
+print(Str[0::2])
+print(Str[::-1])
+
+
+text= str(input("Enter a string: "))
+revText = text [::-1]
+text= revText
+
+print("Reverse of given string is:")
+print(text)
+
+
+
