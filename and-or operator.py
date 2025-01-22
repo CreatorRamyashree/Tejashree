f98b8421b79fd9942d2249c29ab6ea85873aa41c
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-01-22 21:07:10 +0100

    Add files via upload

---
+++ and-or operator.py
@@ -0,0 +1,28 @@
+a= 10
+b= 6
+c= 0
+
+if a and b and c:
+    print("All the numbers have boolean value as true.")
+
+else:
+    print("At least one number has boolean value as false.")
+
+a= 4
+b= -4
+c= 0
+
+if a > 0 or b > 0:
+    print("either of the number is greater than 0.")
+else:
+    print("No number is greater than 0.")
+
+
+
+
+
+
+
+
+
+
---
+++ notequaloperator.py
@@ -0,0 +1,48 @@
+a= 17
+b= 62
+c= 62
+
+print(a != b)
+print(b != c)
+
+a= "Tejashree"
+b= "Ramyashree"
+
+if a != b:
+    print(a, "and", b, "are different")
+
+a= 8
+b= 4
+
+if (a == 1) != (b == 4):
+    print("Codingal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
