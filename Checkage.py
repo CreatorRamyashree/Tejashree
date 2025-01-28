043e76a037389705d636bb42f8dccb6e5a1d7af2
CreatorRamyashree <ramyashree.pinky2011@gmail.com>
2025-01-28 07:46:37 +0100

    Add files via upload

---
+++ Checkage.py
@@ -0,0 +1,12 @@
+age= int(input("Enter the age of the person:"))
+
+if age > 12 :
+    print("You are allowed.")
+    print ("You're Welcome")
+else:
+   if age >= 12 :
+       print("Allowed")
+       print ("You're Welcome")
+   else:
+       print("Not Allowed")
+
